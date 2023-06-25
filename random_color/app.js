ab0e51870ac9a6670a39bcbb63314952e3914584
Jack <jack@Jacks-Mac-mini.attlocal.net>
2023-06-25 13:50:48 -0500

    Adding Random Color Apps

---
+++ random_color/app.js
@@ -0,0 +1,28 @@
+// click
+// change the color
+// random color
+// display that color
+    // text color
+    // rgb color
+
+
+      
+
+
+const color = document.querySelector('.btn')
+color.addEventListener('click', changeColor)
+function changeColor(){
+    
+    document.querySelector('body').style.background = rgb()
+    const p =document.querySelector('p') 
+    p.innerText = rgb ()
+}
+
+
+function rgb(){
+    let r = Math.floor(Math.random() * 256)
+    let g = Math.floor(Math.random() * 256)
+    let b = Math.floor(Math.random() * 256)
+
+    return `rgb(${r},${g}, ${b}`;
+}
