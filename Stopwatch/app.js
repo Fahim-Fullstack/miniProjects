59c94e990cf11aec444090722409d5cf8937b6d8
Jack <jack@Jacks-Mac-mini.attlocal.net>
2023-06-25 14:02:06 -0500

    New Chnages

--- Stopwatch/app.js
+++ Stopwatch/app.js
@@ -5,6 +5,8 @@ const stopTimer = document.querySelector('.btn-danger');
 // reset the timer
 const resetTimer = document.querySelector('.btn-block');
 
+
+
 //minutes
 const minutes = document.querySelector('.minutes') 
 //seconds
@@ -12,10 +14,25 @@ const seconds = document.querySelector('.seconds')
 
 
 
+//Getting the date objects;
 const time = new Date();
 
+
+//Start function
 startTimer.addEventListener('click', startTime)
 function startTime(){
-    alert('hi');
+    
 }
 
+
+
+
+// data-action
+//html arrtibutes
+// start click fucntion
+// stop click fucntion
+// reset click fucntion
+// how to run timer every seconds
+//  timer - iterator 
+// change the innertext to seconds
+// calculate mins (timer /60)  --round the number
