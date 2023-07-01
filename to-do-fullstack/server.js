c96f31ac3f764be113a5908d7cf18f1a18bb1508
Jack <jack@Jacks-Mac-mini.attlocal.net>
2023-06-30 21:44:47 -0500

    adding new changes

--- to-do-fullstack/server.js
+++ to-do-fullstack/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("hello");
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(5000, () => {
